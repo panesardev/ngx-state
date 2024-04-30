0c8d26f145534a3c4d2f160b2c4f16ab2addc246
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-04-30 19:37:29 -0400

    first commit

--- src/app/utilities/operators.ts
+++ src/app/utilities/operators.ts
@@ -1,6 +1,6 @@
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Observable, Subscription } from 'rxjs';
 
-export function runEffects<T>(...sources: Observable<T>[]): Subscription[] {
+export function runEffects(...sources: Observable<unknown>[]): Subscription[] {
   return sources.map(source => source.pipe(takeUntilDestroyed()).subscribe());
 }
